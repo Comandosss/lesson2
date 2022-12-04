8d0b9ed1bdcb6198ef7db11c2bd36bb47a43ada9
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2022-12-04 19:03:54 +0300

    Удалил функцию, добавил: проверку на пустоту списка, запятых; изменил f-строки

--- 3_for.py
+++ 3_for.py
@@ -17,42 +17,42 @@
 """
 
 
-def main(each_product):
-    summa_each = sum(each_product, 0)
-    return summa_each
-
-
 if __name__ == "__main__":
     table_sales = [
         {
             'product': 'iPhone 12',
             'items_sold': [
-                363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186
+                363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186,
             ]
         },
         {
             'product': 'Xiaomi Mi11',
             'items_sold': [
-                317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316
+                317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316,
             ]
         },
         {
             'product': 'Samsung Galaxy 21',
             'items_sold': [
-                343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247
+                343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247,
             ]
         },
     ]
-    sum_all_products = 0
+    sum_all = 0
     sum_len_lists = 0
     for count in table_sales:
         product = count['product']
-        sales_count = count['items_sold']
-        print(f'Прод: {product}, сумм. кол-во продаж: {main(sales_count)} шт.')
-        avg_each_prod = main(count['items_sold']) // len(count['items_sold'])
-        print(f'Прод: {product}, среднее кол-во продаж: {avg_each_prod} шт.')
-        sum_all_products += main(sales_count)
-        sum_len_lists += len(sales_count)
-    print(f'Сумм. кол-во продаж всех товаров: {sum_all_products} шт.')
-    avg_all_product = sum_all_products // sum_len_lists
-    print(f'Среднее кол-во продаж всех товаров: {avg_all_product} шт.')
+        each_len_list = len(count['items_sold'])
+        if count['items_sold']:
+            sum_each = sum(count['items_sold'], 0)
+            print(f'Прод: {product}, сумм. кол-во продаж: {sum_each} шт.')
+            avg_each = sum_each // each_len_list
+            print(f'Прод: {product}, сред. кол-во продаж: {avg_each} шт.')
+            sum_all += sum_each
+            sum_len_lists += each_len_list
+        else:
+            print(f'Прод: {product}, сумм. кол-во продаж: 0 шт.')
+            print(f'Прод: {product}, сред. кол-во продаж: 0 шт.')
+    print(f'Сумм. кол-во продаж всех товаров: {sum_all} шт.')
+    avg_all = sum_all // sum_len_lists
+    print(f'Сред. кол-во продаж всех товаров: {avg_all} шт.')
